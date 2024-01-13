6ed2fca4151b3463c4c7f4ae331ac878cce60be0
Nida Rizwan <143074620+NidzR@users.noreply.github.com>
2024-01-14 03:29:43 +0500

    NODEJS Assignment Q21
    21. They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.

---
+++ Q21.js
@@ -0,0 +1,19 @@
+"use strict";
+// Q21.They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+Object.defineProperty(exports, "__esModule", { value: true });
+let user = {
+    fName: "Rizwan",
+    Lname: "Akhtar",
+    age: 40,
+    hobbies: ["study", "paintings", "long drive"],
+    salary: () => 30000,
+};
+user.email = "abc@abc.com",
+    user.adress = "1234 block 2",
+    // // delete user.salary
+    // // delete user.email
+    // delete user.adress
+    console.log("email" in user);
+console.log("adress" in user);
+console.log(user);
+console.log(user.salary());
